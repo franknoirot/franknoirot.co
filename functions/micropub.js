20e0ceabd637b12d8d35a51f4fb4aeea1933dc13
franknoirot <frankjohnson1993@gmail.com>
2020-08-29 07:49:12 -0400

    Completed MicroPub Netlify Function endpoint!

--- functions/micropub.js
+++ functions/micropub.js
@@ -1,59 +1,109 @@
-const Octokit = require('@octokit/rest')
+const multipart = require('parse-multipart')
+const { Octokit } = require('@octokit/rest')
 const fetch = require('isomorphic-fetch')
-const qs = require('querystring')
 
 const OWNER = 'franknoirot'
 const REPO = 'franknoirot.co'
-const POST_FOLDER = './posts'
+const POST_FOLDER = 'posts'
 
+const now = new Date()
+const padZ = (int, zeros) => int.toString().padStart(zeros, '0')
+const date = `${now.getFullYear()}-${padZ(now.getMonth(), 2)}-${padZ(now.getDate(), 2)}`
+
+function postMeta(title, slug) {
+    return `---\ntitle: "${ title }"\nslug: "${ slug }"\nlayout: "layouts/post.njk"\ndate: ${ now.toISOString() }\ntags:\n  - post\n  - code\n---\n\n`
+}
 
 exports.handler = async function (event, context, callback) {
-    console.log('event', event)
-
-    // get post's content and user's auth info
-    const postJSON = qs.parse(event.body)
-
-    console.log('data is ', postJSON)
-
-    // verify user's auth token via IndieAuth
-    const authRes = await fetch('https://tokens.indieauth.com/token', {
-        method: 'GET',
-        headers: {
-            'Accept': 'application/json',
-            'Authorization': event.headers.Authorization,
-        }
-    }).then(res => await res.json())
-    .catch(err => console.error(err))
-
-    console.log('authRes = ', authRes)
-
-    // if invalid user, return failure state
-    if (!authRes || !authRes.hasOwnProperty('me') || !authRes.me || !postJSON.hasOwnProperty('content') || !postJSON.content) {
-        callback(new Error("malformed authentication"), {
-            statusCode: 401,
-            body: "Unauthorized: malformed authentication",
+    if (event.method === 'OPTIONS') {
+        callback(null, {
+            statusCode: 200,
+            headers: {
+                'Control-Access-Allow-Origin': '*',
+            }
         })
-
-        return 
     }
 
-    // if valid user, add contents to GitHub repo as a Markdown (or HTML) file
-    const octo = new Octokit({
-        auth: process.env.GITHUB_TOKEN,
-    })
+    try {
+        // get post's content and user's auth info
+        const contentType = event.headers['content-type']
+        const boundary = contentType.match(/boundary=(.*)$/)[1]
 
-    // push commit to GitHub repo
-    await uploadToRepo(octo, POST_FOLDER, postJSON.content, OWNER, REPO)
+        const parts = Object.fromEntries(event.body.split(boundary).map(partStr => {
+            const key = partStr.match(/name="(.*)"/)
+            const value = partStr.match(/name=".*"\r\n\r\n((.|\n)*)/)
+            if (key && value) {
+                return [key[1],value[1]]
+            } else {
+                return false
+            }
+        }).filter(x => x && x !== null))
 
-    callback(null, {
-        statusCode: 201,
-        body: 'Post created! Site rebuilding',
-    })
+        // // verify user's auth token via IndieAuth
+        // const authRes = await fetch('https://tokens.indieauth.com/token', {
+        //     method: 'GET',
+        //     headers: {
+        //         'Accept': 'application/json',
+        //         'Authorization': event.headers.Authorization,
+        //     },
+        // }).catch(err => console.error(err))
+
+        // uhh Netlify Identity just comes for free with the request????
+        const { user } = context.clientContext
+
+        // if invalid user, return failure state
+        if (!user || !user.hasOwnProperty('me') || !user.me || !parts.hasOwnProperty('content') || !parts.content) {
+            callback(new Error("malformed authentication"), {
+                statusCode: 403,
+                body: "Forbidden: malformed authentication",
+            })
+
+            return 
+        }
+
+        // if valid user, add contents to GitHub repo as a Markdown (or HTML) file
+        const octo = new Octokit({
+            auth: process.env.GITHUB_TOKEN,
+        })
+
+        let title = parts.title
+        if (!title) {
+            title = parts.content.match(/# (.*)\n\n/)
+        }
+        
+        if (title) {
+            title = title[1]
+            parts.content = parts.content.replace(/# (.*)\n\n/, '')
+        } else {
+            title = `Post ${date}`
+        }
+
+        parts.content = postMeta(title, parts.slug) + parts.content
+
+        const filePath = POST_FOLDER + '/' + ((parts.slug) ? parts.slug + '.md' : `${date}--${now.getHours}-${now.getMinutes}-${now.getSeconds}.md`)
+
+        console.log(parts.content)
+
+        // push commit to GitHub repo
+        await uploadToRepo(octo, filePath, parts.content, OWNER, REPO)
+
+        callback(null, {
+            statusCode: 201,
+            body: 'Post created! Site rebuilding',
+        })
+    } catch(err) {
+        console.error(err)
+        callback(err, {
+            statusCode: 502,
+            body: JSON.stringify(err),
+        })
+    }
 } 
 
 async function uploadToRepo(octo, path, content, owner, repo, branch = `master`) {
     const currCommit = await getCurrCommit(octo, owner, repo, branch)
-    const fileBlob = await createBlobForFile(octo, content, org, repo)
+    const fileBlob = await createBlobForFile(octo, content, owner, repo)
+    console.log('fileBlob = ', fileBlob)
     const newTree = await createNewTree(
         octo,
         owner,
@@ -62,6 +112,7 @@ async function uploadToRepo(octo, path, content, owner, repo, branch = `master`)
         path,
         currCommit.treeSha
     )
+    console.log('newTree = ', newTree)
     const commitMessage = `New post published via MicroPub!`
     const newCommit = await createNewCommit(
         octo,
@@ -71,6 +122,7 @@ async function uploadToRepo(octo, path, content, owner, repo, branch = `master`)
         newTree.sha,
         currCommit.commitSha,
     )
+    console.log('newCommit = ', newCommit)
 
     await setBranchToCommit(octo, owner, repo, branch, newCommit.sha)
     console.log('Done!')
@@ -94,7 +146,7 @@ async function getCurrCommit(octo, owner, repo, branch = `master`) {
     }
 }
 
-async function createBlobForFile(octo, content, org, repo) {
+async function createBlobForFile(octo, content, owner, repo) {
     const blobData = await octo.git.createBlob({
         owner,
         repo,
@@ -104,7 +156,7 @@ async function createBlobForFile(octo, content, org, repo) {
     return blobData.data
 }
 
-function createNewTree(octo, owner, repo, blob, path, parentTreeSha) {
+async function createNewTree(octo, owner, repo, blob, path, parentTreeSha) {
     const tree = [{
         path,
         mode: `100644`,
@@ -121,7 +173,7 @@ function createNewTree(octo, owner, repo, blob, path, parentTreeSha) {
 }
 
 async function createNewCommit(octo, owner, repo, message, currTreeSha, currCommitSha) {
-    (await octo.git.createCommit({
+    return (await octo.git.createCommit({
         owner,
         repo,
         message,
